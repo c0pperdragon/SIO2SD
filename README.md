3c49a51a56ec4a3789810ae2f8bb2b7ca5cca005
c0pperdragon <reinhard.grafl@aon.at>
2018-03-17 20:43:25 +0100

    Changed user interface

--- README.md
+++ README.md
@@ -1,47 +1,45 @@
 # SIO2SD
-An Arduino-based floppy drive emulator for the atari 8 bit computers.
+An Arduino-based floppy drive emulator for the Atari 8 bit computers.
 
-Instead of just buying a complete SIO2SD device, I decided to build my own using my trusty Arduino Micro. 
-This seems to have just the right number of pins for the purpose as well as the necessary interfaces. 
-Support for an SDCARD library is already included in the Arduino IDE.
+This small Arduino build emulates an Atari 1050 floppy drive to connect 
+to an original Atari 8-bit computer. The program is compatible with 
+most Arduino boards. And for boards based on the Atmega32u4 it also provides
+various log messages on the serial monitor to help debugging (mainly for my own use).
 
-I used my own technique to manually wire all components to a board instead of creating a PCD for which I 
-do not have the proper equipment. This task is quite time-consuming but much fun nevertheless.
-The board layout file does not accurately contain all the components because I did not find them in 
-the eagle library. So I just used generic pin-headers to denote the position of the female Arduino
-socket and the female SDCARD connector socket. For some reasons the eagle library not even contains a 7-segment
-digit display in the correct format, so again I used pin-headers here.
+For a breadboard setup like my development device, you will need two push buttons, 
+two 7-digit LEDs and one SDCard socket (with built-in or separate level shifters) and
+some wires and resistors. The connectivity is pretty standard and you will find
+the details as comments in the source code.
 
-Instead of a standard SDCARD-socket I wired up one of these micro-SD-adapters to form a kind of breakout adapter
-for a micro-SD card. By making it detachable I can probably use it for later prototyping projects. 
+## Emulated floppies on SDcard
+The floppy drive emulator needs an SDcard containing .ats files for each individual floppy disk.
+Because there is no elaborated user-interface on the device, the naming and location of the files 
+need to follow a fixed system: The files are searched for in a directory ATARI/
+on the root of the SDcard. Every disk file must be named nn_Anything.ats with two leading digits that correspond to the 
+numeric display on the device and the mandatory '_' character. 
+Currently only a very basic support for single density (maximum 720 sectors) 
+and enhanced density (1040 sectors) floppies is provided without special functions like
+formatting or any of the commands of third-party devices.
 
-For the connection to the weird Atari SIO socket, I just built a my own wire connection from 5 individual pin-connectors
-that fit on the 1-millimeter pins inside the socket. This looks a bit strange, but works very reliably.
+## User interface
+You can switch though the numbered disks 00 to 99 with the two buttons. This is the maximum number of disks 
+supported on a single SDcard. When you switch beyond 99, you will see -- on the display, which means the device
+now emulates a switched off floppy drive. 
+You can also emulate a second floppy drive, which is very usable for example for holding the save disks
+for games that support this. To do so, you need to press both buttons to make the small indicator dot appear
+on the other LCD block. This means that the number you see in the display is the floppy that is inserted
+in drive 2. This floppy can now also be changed with the two buttons. It is even possible to select the same 
+floppy in both drives at once (but this is probably of no great use).
 
-How to use the device:
-After being connected to the 5 correct pins from the atari port which also provides the power to drive the Arduino,
-the device needs an SDCARD containing the necessary disk files. Currently only .ats files with a sector size of 128 byte 
-(which seems to be the standard) are supported. Because there is no elaborated user-interface on the device, 
-the naming and location of the files need to follow a fixed system: The files are searched for in a directory ATARI/
-on the root of the SDCARD. Every disk file must be named nn_Anything.ats with two leading digits that correspond to the 
-numeric display on the device and the mandatory '_' character. Two additional concurrent disk drives will be emulated when there 
-are files with name nnBAnything.ats up to nnCAnything.ats. This can be useful to provide writeable save-disks for certain games.
-D4 is not emulated, so it is possible to connect a real floppy drive together with this emulator to do file transfers. 
-The total number of selectable configurations is 100 which is limited by the two digit display.
-If more disk files are needed, they must be stored on another SDCARD.
-With the buttons the number in the display can be changed to select a different disk file / set of files. 
-If no file for the number can be found, the device emulates an empty floppy drive. 
 
-Have a look at the gallery image to see how the parts are made and how everything connects.
-
-## New Hardware Variants
+## Hardware Variants
 
 * First prototype with Arduino Micro. 
 This was hand-wired. No longer suppported by the firmware.
 
-* Bare bone ATmega328
+* Bare bone ATmega328 (DIP package)
 Designed to fit into a small form factor. MCU is running at 8MHz on 3.3V and can talk to SDCard without level shifters. 
 LEDs have resistors on the digit select lines only, therefore the brightness various with the pattern.
 
-* Breadboard setup with Arduino Nano
-Having 8 resistors for the LED and a level shifter to the SDCard interface.
+* Breadboard setup with Arduino Micro
+Also having only 2 resistors for the LEDs and a level shifter to the SDCard interface.
--- src/AtariSIO2SD/AtariSIO2SD.ino
+++ src/AtariSIO2SD/AtariSIO2SD.ino
@@ -26,9 +26,9 @@
 
 // ------------------------ the wiring up of all components ------------------------
 // -- connection to Atari SIO port
-                              // default RX - serial from ATARI
-                              // default TX - serial from Atari
-#define PIN_SIOCOMMAND  2     // CMD signal from Atari
+                              // default arduino RX -> serial data from Atari SIO pin 5
+                              // default arduino TX -> serial data to Atari SIO pin 3
+#define PIN_SIOCOMMAND  2     //                    -> CMD signal from Atari SIO pin 7
 
 // -- connection to SDCARD
                               // default MOSI
@@ -54,20 +54,20 @@ byte pin_segments1[8] = {4,  5,  A5, A0, A1, A3, A2, 6};
 // because the two digits are displayed in a multiplexted fashion (to reduce number of needed pins),
 // they will be displayed alternately. a timer interrupt  comes handy to do this.
 
-#define EEPROM_ADDRESS  99
+volatile byte selecteddisk[2];
+volatile int activitylight[2];
+#define ACTIVITY_LIGHT_DURATION 10
+
+byte activedigit; 
+byte showndiskdrive;
+int valuesavedelay;
 
 byte prevbuttonstate;
 byte timesincebuttonchange;
-
-volatile byte digit0value;  
-volatile byte digit1value;  
-volatile int activitylight;
-int valuesavedelay;
-int savelight;
-byte activedigit; 
+bool inputlock;
 
 // various digits             gfedcba
-byte digitpatterns[10] = 
+byte digitpatterns[11] = 
 {   B00111111,   // 0
     B00000110,   // 1
     B01011011,   // 2
@@ -77,7 +77,8 @@ byte digitpatterns[10] =
     B01111101,   // 6
     B00000111,   // 7
     B01111111,   // 8
-    B01101111    // 9
+    B01101111,   // 9
+    B01000000,   // -
 };
 
 void initdiskselector()
@@ -89,20 +90,20 @@ void initdiskselector()
     pinMode(PIN_BUTTON1, INPUT_PULLUP);
     pinMode(COMMON_ANODE_SELECTOR, INPUT_PULLUP);
     
+  
+    // read selector values at startup
+    selecteddisk[0] = read_eeprom(0);
+    selecteddisk[1] = read_eeprom(1);
+  
+    // init various UI flags
+    activitylight[0] = 0;
+    activitylight[1] = 0;
+    activedigit = 0;
+    showndiskdrive = 0;
+    valuesavedelay = 0;
     prevbuttonstate = 0;
     timesincebuttonchange = 0;
-  
-    // read selector value at startup
-    byte s = EEPROM.read(EEPROM_ADDRESS);
-    if (s>99) { s=0; }
-  
-    // init visible digit values and toggle register
-    digit0value = s % 10;
-    digit1value = s / 10;
-    activedigit = 0;
-    valuesavedelay = 0;
-    savelight = 0;
-    activitylight = 0;
+    inputlock = false;
   
     // configure and turn of digit selectors 
     pinMode(PIN_DIGIT0, OUTPUT);
@@ -121,99 +122,180 @@ void initdiskselector()
     Timer1.attachInterrupt(polldiskselector);    
 }
 
-int getdiskselectorvalue()
-{
-    return digit0value + digit1value*10;  
-}
-
-int setactivitylight()
-{
-    activitylight = 10;  
-}
-
 // interrupt service routine is called 200 times per second 
 // (must not take much time)
 // this handles user input and also drives the LED digits
 void polldiskselector()   
 {
-    byte buttonstate = 0;
-    byte commoncathode;
+    // poll buttons only times per second to reduce bumping
+    if (activedigit==0) 
+    {   byte buttonstate = 0;
     
-    // --- query current button states ---
-    if (digitalRead(PIN_BUTTON0)==LOW) buttonstate =  buttonstate | (1<<0);
-    if (digitalRead(PIN_BUTTON1)==LOW) buttonstate =  buttonstate | (1<<1);
-
-    if (buttonstate!=prevbuttonstate)
-    {   prevbuttonstate = buttonstate;
-        timesincebuttonchange=0;
-    }
-    // cause buttons to modify digits (either at first press or on repeat)
-    if (timesincebuttonchange==0 || timesincebuttonchange==200 && buttonstate!=0) 
-    {   // modify digits without needing to do divisions (must be fast)
-        if (buttonstate&1) 
-        {   digit0value = digit0value<9 ? digit0value+1 : 0;
-            if (digit0value==0) { digit1value = digit1value<9 ? digit1value+1 : 0; }
+        // --- query current button states and detect changes ---
+        if (digitalRead(PIN_BUTTON0)==LOW) { buttonstate =  buttonstate | 1; }
+        if (digitalRead(PIN_BUTTON1)==LOW) { buttonstate =  buttonstate | 2; }
+    
+        // -- detect a press of both buttons: switch shown disk drive 
+        if ((buttonstate&3)==3 && (prevbuttonstate&3)!=3 && !inputlock)
+        {   showndiskdrive = 1-showndiskdrive;
+            inputlock = true;
         }
-        else if (buttonstate&2) 
-        {   digit0value = digit0value>0 ? digit0value-1 : 9;            
-            if (digit0value==9) { digit1value = digit1value>0 ? digit1value-1 : 9; }
+        if (prevbuttonstate==0 && buttonstate==0) { inputlock=false; }
+    
+        // -- detect clicks of single up and down buttons and provide key repeat function
+        if (!inputlock)
+        {   if ( ((buttonstate&1)==0 && (prevbuttonstate&1)!=0) || ((buttonstate&1)!=0 && timesincebuttonchange>=100) ) 
+            {   selecteddisk[showndiskdrive] ++;
+                if (selecteddisk[showndiskdrive]>100) selecteddisk[showndiskdrive]=0;  
+                valuesavedelay = 100;    
+            }
+            if ( ((buttonstate&2)==0 && (prevbuttonstate&2)!=0) || ((buttonstate&2)!=0 && timesincebuttonchange>=100) ) 
+            {   selecteddisk[showndiskdrive] --;
+                if (selecteddisk[showndiskdrive]>100) selecteddisk[showndiskdrive]=100;      
+                valuesavedelay = 100;    
+            }
+        }    
+    
+        // -- memorize current button state for next poll to then detect edges
+        if (buttonstate!=prevbuttonstate)
+        {   prevbuttonstate = buttonstate;
+            timesincebuttonchange=0;
+        }          
+        else
+        {   if (timesincebuttonchange<100) { timesincebuttonchange++; }
+            else                           { timesincebuttonchange-=5; }
         }
-        valuesavedelay = 600; // 3 seconds before save
-    }
-    // measure the time the buttons are in current state (and use it for key-repeat)
-    if (timesincebuttonchange<200) { timesincebuttonchange++; }
-    else                           { timesincebuttonchange-=10; }
- 
-    // --- handling of delay before saving the current selected value
-    if (valuesavedelay>0)
-    {   valuesavedelay --;
-        if (valuesavedelay==0)
-        {   EEPROM.update(EEPROM_ADDRESS, digit0value + digit1value*10);
-            savelight = 200;
+        
+        // --- handling of delay before saving the current selected value
+        if (valuesavedelay>0 && buttonstate==0)
+        {   valuesavedelay --;
+            if (valuesavedelay==0)
+            {   store_eeprom(0, selecteddisk[0]);
+                store_eeprom(1, selecteddisk[1]);
+            }
         }
     }
+    
 
     // --- count down activity light duration
-    if (activitylight>0) {   activitylight--;  }
-
-    // --- count down save light duration
-    if (savelight>0) { savelight--; }
+    if (activitylight[0]>0) {   activitylight[0]--;  }
+    if (activitylight[1]>0) {   activitylight[1]--;  }
   
     // --- display digits alternatingly ----
     // switch to other digit
     activedigit = 1-activedigit;
 
     // determine if need to drive a common cathode or common anode LED
-    commoncathode = digitalRead(COMMON_ANODE_SELECTOR);
+    byte commoncathode = digitalRead(COMMON_ANODE_SELECTOR);
   
     // turn on/off digit segments as needed (pin assignment is quite arbitrary)
     if (activedigit==0) 
-    {   digitalWrite (PIN_DIGIT1, commoncathode ? HIGH : LOW);  // turn off digit 1
+    {   byte value = selecteddisk[showndiskdrive];
+        value = (value<=99) ? (value % 10) : 10;   // when drive is off: - 
+        digitalWrite (PIN_DIGIT1, commoncathode ? HIGH : LOW);  // turn off digit 1
         byte i;
-        byte v = digitpatterns[digit0value];
+        byte v = digitpatterns[value];
         for (i=0; i<7; i++) 
         {   if (v&(1<<i)) { digitalWrite (pin_segments0[i], commoncathode ? HIGH : LOW); }
             else          { digitalWrite (pin_segments0[i], commoncathode ? LOW : HIGH); }
         }
-        // this DOT is used to show activity 
-        if (activitylight>0) { digitalWrite (pin_segments0[7], commoncathode ? HIGH : LOW); }
-        else                 { digitalWrite (pin_segments0[7], commoncathode ? LOW : HIGH); }
+        // this dot is used to see if drive 0 is visible and if activity on disk 0
+        bool dot = showndiskdrive==0;
+        if (activitylight[0]>0) { dot = !dot; }
+        if (dot) { digitalWrite (pin_segments0[7], commoncathode ? HIGH : LOW); }
+        else     { digitalWrite (pin_segments0[7], commoncathode ? LOW : HIGH); }
         digitalWrite (PIN_DIGIT0, commoncathode ? LOW : HIGH);  // turn on digit 0
     }
     else 
-    {   digitalWrite (PIN_DIGIT0, commoncathode ? HIGH  : LOW);   // turn off digit 0
+    {   byte value = selecteddisk[showndiskdrive];
+        value = (value<=99) ? (value / 10) : 10;   // when drive is off: -
+        digitalWrite (PIN_DIGIT0, commoncathode ? HIGH  : LOW);   // turn off digit 0
         byte i;
-        byte v = digitpatterns[digit1value];
+        byte v = digitpatterns[value];
         for (i=0; i<7; i++) 
         {   if (v&(1<<i)) { digitalWrite (pin_segments1[i], commoncathode ? HIGH : LOW); }
             else          { digitalWrite (pin_segments1[i], commoncathode ? LOW : HIGH); }
         }
-        // the DOT is used to show settings saving 
-        if (savelight>0) { digitalWrite (pin_segments1[7], commoncathode ? HIGH : LOW); }  
-        else             { digitalWrite (pin_segments1[7], commoncathode ? LOW : HIGH); }
+        // this dot is used to see if drive 1 is visible and if activity on disk 1
+        bool dot = showndiskdrive==1;
+        if (activitylight[1]>0) { dot = !dot; }
+        if (dot) { digitalWrite (pin_segments1[7], commoncathode ? HIGH : LOW); } 
+        else     { digitalWrite (pin_segments1[7], commoncathode ? LOW : HIGH); } 
         digitalWrite (PIN_DIGIT1, commoncathode ? LOW : HIGH);  // turn on digit 1
     }  
 }
+
+// ------------------ EEPROM storage with wear-levelling -----------
+// To prevent frequent writing to a single EEPROM cell to wear out this particual storage,
+// the writes are distributed over a wide range of the available EEPROM. This is
+// possible because only a very tiny amount of data needs to be stored int each slot
+// (a 7-bit value).
+// It works as follows: A splot consists of 256 bytes EEPROM storage. The one active
+// data value (7 bits) can be found on the first byte position that has a different most significant
+// bit (MSB) value than all previous bytes. In the case that all bytes in the slot have the
+// same MSB, the very first byte is taken.
+// Writing a new value into the slot will be done by overwriting the byte just before
+// the previous value-carrying byte with the proper MSB set. 
+// By this only one byte is overwritten in each write operation and it will always be a
+// different one in the slot.
+
+byte read_eeprom(byte slot)
+{
+    // find the first different MSB and take the value from there
+    int start = ((int)slot) * 256;
+    byte b0 = EEPROM.read(start);
+    int i;
+    for (i=start+1; i<start+256; i++)
+    {   byte b1 = EEPROM.read(i);
+        if ( (b0&0x80) != (b1&0x80)) { return b1 & 0x7f; }
+        b0 = b1;
+    }
+    return EEPROM.read(start) & 0x7f;
+}
+
+void store_eeprom(byte slot, byte value)
+{
+    // make sure to not store unnecessarily 
+    if (read_eeprom(slot)==value) { return; }
+
+    // find the first different MSB 
+    int start = ((int)slot) * 256;
+    byte b0 = EEPROM.read(start);
+    int i;
+    for (i=start+1; i<start+256; i++)
+    {   byte b1 = EEPROM.read(i);
+        // if found a different MSB, store the value just in the place before
+        if ( (b0&0x80) != (b1&0x80)) 
+        {   EEPROM.write(i-1, (value&0x7f) | (b1&0x80));
+            PRINT(F("Stored "));
+            PRINT(value);
+            PRINT(F(" to EEPROM byte "));
+            PRINTLN(i-1);
+            // when having written to location 0 make sure to have no other MSB edges 
+            if (i==1) { cleanmsb_eeprom(slot); }
+            return;
+        }
+        b0 = b1;
+    }
+    // if no suitable place could be found (all MSB equal, start at the end)
+    EEPROM.write(start+255, (value&0x7f) | ((~b0)&0x80)); 
+}
+
+void cleanmsb_eeprom(byte slot)
+{
+    int start = ((int)slot) * 256;
+    byte b0 = EEPROM.read(start);
+    int i;
+    for (i=start+1; i<start+256; i++)
+    {   byte b1 = EEPROM.read(i);
+        if ( (b0&0x80)!=(b1&0x80) )
+        {   EEPROM.write(i, (b0&0x80)|(b1&0x7f)); 
+            PRINT(F("Cleaned EEPROM byte "));
+            PRINTLN(i);            
+        }
+    }
+}
+
 
 
 // ---------------------- DISK FILE HANDLING  -----------------------
@@ -223,10 +305,10 @@ bool didinitsd = false;
 File diskfile;  
 unsigned int disksize;         // size in sectors
 
-void opendiskfile(byte drive, int index)
+void opendiskfile(int index)
 {  
     // check if desired file is already open - continue to use it
-    if (diskfile && isrequesteddiskfile(diskfile,drive,index)) { return; } 
+    if (diskfile && isrequesteddiskfile(diskfile,index)) { return; } 
   
     // before switching disk file, close previous one if still open
     if (diskfile) { diskfile.close(); }
@@ -256,14 +338,12 @@ void opendiskfile(byte drive, int index)
     {   File entry = root.openNextFile();
         if (!entry)
         {   PRINT(F("Could not find file for disk "));
-            PRINT(index);
-            PRINT(F(" in drive "));
-            PRINTLN(drive);
+            PRINTLN(index);
             root.close();
             return;
         }
         // check if name matches
-        if (isrequesteddiskfile(entry,drive,index))
+        if (isrequesteddiskfile(entry,index))
         { // fill data into the fullname variable (which terminates the loop)
             strcat (fullname, "ATARI/");
             strcat (fullname, entry.name());
@@ -331,12 +411,11 @@ void opendiskfile(byte drive, int index)
     else          { PRINTLN(F(" (writeable)")); }
 }
 
-bool isrequesteddiskfile(File f, byte drive, int index)
+bool isrequesteddiskfile(File f, int index)
 {
     char* n = f.name();
-    if (n[0]>='0' && n[0]<='9' && n[1]>='0' && n[1]<='9' && ((n[0]-'0')*10 + n[1]-'0')==index) 
-    {   if (n[2]=='_' && drive==0) { return true; }
-        if (n[2]>='B' && n[2]<='H' && n[2]-'A'==drive) { return true; }
+    if (n[0]>='0' && n[0]<='9' && n[1]>='0' && n[1]<='9' && ((n[0]-'0')*10 + n[1]-'0')==index && n[2]=='_')
+    {   return true; 
     }
     return false;
 }
@@ -434,45 +513,46 @@ void handlecommand_status()
     sendwithchecksum(status,4);
 }
 
-void handlecommand_read(unsigned int sector)
-{
-    setactivitylight();
- 
+bool handlecommand_read(unsigned int sector)
+{ 
     byte data[128];
     if (!readsector(sector,data))
     {   SIO.write('E');
-        return;
+        return false;
     }      
     SIO.write('C');
     sendwithchecksum(data, 128);    
+    return true;
 }
 
-void handlecommand_write(unsigned int sector)
+bool handlecommand_write(byte drive, unsigned int sector)
 { 
     byte data[128];
     if (!receivewithchecksum(data,128))
     {   PRINTLN(F("Received sector with invalid checksum"));
         SIO.write('E');
-        return;
+        return true;
     } 
 
     delay(1);
     SIO.write('A');
-   
+
+    opendiskfile(selecteddisk[drive]);   
     if (!writesector(sector,data))
     {   PRINTLN(F("Writing sector to SD card failed"));
         SIO.write('E');
-        return;
+        return false;
     }      
   
     delay(1);      
     SIO.write('C');
+    return true;
 }
 
 void handle_sio()
 {
     int i;
-    unsigned int drive;
+    byte drive;
     unsigned int sector;
     
     // must wait until the command line goes low 
@@ -535,20 +615,26 @@ void handle_sio()
     {   PRINTLN(F("Ignored command with invalid checksum"));
         return;
     }
-    // when the command is not intended for floppy device D1 to D3, ignore it
-    if (command[0]<0x31 || command[0]>0x33)
+    // when the command is not intended for floppy device D1 or D2, ignore it
+    if (command[0]<0x31 || command[0]>0x32)
     {   PRINT(F("Received command for different device "));
         PRINTLN(command[0], HEX);
         return;       
     }
-    
+    drive = command[0]-0x31;
+    // when the floppy drive is switched to "-" it is meant to be turned off
+    if (selecteddisk[drive]>99)
+    {   PRINT(F("Received command for turned off disk drive "));
+        PRINTLN(command[0], HEX);
+        return;       
+    }
+        
     switch (command[1])
     {   case 0x53:  // STATUS
         {   delay(1);
             SIO.write('A');
             delay(2);
-            drive = command[0]-0x31;
-            opendiskfile(drive, getdiskselectorvalue());
+            opendiskfile(selecteddisk[drive]);
             handlecommand_status();
             break;
         }
@@ -556,20 +642,20 @@ void handle_sio()
         {   delay(1);
             SIO.write('A');
             delay(2);
-            drive = command[0]-0x31;
             sector = ((unsigned int)command[2]) + (((unsigned int)command[3])<<8) - 1;
-            opendiskfile(drive, getdiskselectorvalue());
-            handlecommand_read(sector);
+            opendiskfile(selecteddisk[drive]);
+            if (handlecommand_read(sector))
+            {   activitylight[drive] = ACTIVITY_LIGHT_DURATION;
+            }
             break;
         }
         case 0x57:   // WRITE WITH VERIFY
         {   delay(1);
-            SIO.write('A');
-            delay(2);
-            drive = command[0]-0x31;
             sector = ((unsigned int)command[2]) + (((unsigned int)command[3])<<8) - 1;
-            opendiskfile(drive, getdiskselectorvalue());
-            handlecommand_write(sector);
+            SIO.write('A');
+            if (handlecommand_write(drive,sector))
+            {   activitylight[drive] = ACTIVITY_LIGHT_DURATION;
+            }
             break;
         }
         default:
